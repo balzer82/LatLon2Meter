1c89c7acbe4dbbb9966d402dfa9ec4bc4ec6dbeb
balzer82 <balzer82@gmail.com>
2014-03-07 08:19:00 +0100

    Meters to Lat/Lon

--- LatLon2Meter.py
+++ LatLon2Meter.py
@@ -8,7 +8,8 @@ import requests
 import sys, os
 import matplotlib.pylab as plt
 from mpl_toolkits.basemap import Basemap
-#print('Matplotlib Version %s' % plt.__version__)
+print('Matplotlib Version %s' % plt.__version__)
+%pylab inline --no-import-all
 
 # <headingcell level=2>
 
@@ -64,6 +65,7 @@ else:
     # Ziel
     lat2 = 52.5075419
     lon2 = 13.4261418
+
     print('Usage: %s latfrom lonfrom latto lonto (in Decimaldegree)' % sys.argv[0])
     
 sadr = getadress(lat1, lon1)
@@ -80,21 +82,31 @@ lon0=(lon1+lon2)/2.0
 
 # <markdowncell>
 
-# Abstand zwischen zwei Breitenkreisen ist ca. $111.3 \frac{km}{\circ}$.
-# Am Äquator ist die Entfernung zwischen zwei Längenkreisen ebenfalls $111.3 \frac{km}{\circ}$, an den Polen allerdings $0 \frac{km}{\circ}$. Daher muss $dx$ mit $\Delta Lon \cdot \cos(Lat)$ berechnet werden.
+# Abstand zwischen zwei Breitenkreisen ist ca. $111.32 \frac{km}{\circ}$.
+# Am Äquator ist die Entfernung zwischen zwei Längenkreisen ebenfalls $111.32 \frac{km}{\circ}$, an den Polen allerdings $0 \frac{km}{\circ}$. Daher muss $dx$ mit $\Delta Lon \cdot \cos(Lat)$ berechnet werden.
 
 # <codecell>
 
-plt.plot(np.arange(0,90.0, 1), 111.3*np.cos(np.arange(0,90.0*np.pi/180.0, 1.0*np.pi/180.0)))
+arc = 111.323872 # km/°
+
+# <codecell>
+
+plt.plot(np.arange(0,90.0, 1), arc*np.cos(np.arange(0,90.0*np.pi/180.0, 1.0*np.pi/180.0)))
 plt.xlabel('Lat in $^\circ$')
 plt.ylabel('$km/^\circ$ Lon')
+plt.annotate('111.32km', (10.0, 111.32),xycoords='data', \
+            xytext=(30.0, 111.32), textcoords='data', va='center', arrowprops=dict(arrowstyle="simple"))
+plt.annotate('Equator', (0.0, 0.0),xycoords='data', \
+            xytext=(0.0, -20.0), textcoords='data', ha='center', va='center', arrowprops=dict(arrowstyle="fancy"))
+plt.annotate('Northpole', (90.0, 0.0),xycoords='data', \
+            xytext=(90.0, -20.0), textcoords='data', ha='center', va='center', arrowprops=dict(arrowstyle="fancy"))
 
 # <codecell>
 
 lat = lat0 * np.pi/180.0 # to RAD for Cos()
 
-dx = 111.3 * np.cos(lat) * (lon1-lon2)
-dy = 111.3 * (lat1 - lat2)
+dx = arc * np.cos(lat) * (lon2-lon1)
+dy = arc * (lat2 - lat1)
 dist = np.sqrt(dx**2 + dy**2)
 print('Entfernung aus Pythagoras:\t\t%.3fm (ca. %dkm)' % (dist*1000.0, dist))
 
@@ -105,14 +117,34 @@ print('Entfernung aus Pythagoras:\t\t%.3fm (ca. %dkm)' % (dist*1000.0, dist))
 # <markdowncell>
 
 # $$r_\text{Erde}=6378.388km$$
-# $$d=r_\text{Erde} \cdot \arccos\left(\sin(lat1)\sin(lat2) + \cos(lat1)\cos(lat2)\cos(lon2-lon1)\right)$$
+# $$d=r_\text{Erde} \cdot \arccos\left(\sin(lat_1)\cdot \sin(lat_2) + \cos(lat_1)\cdot \cos(lat_2)\cdot \cos(lon_2-lon_1)\right)$$
 
 # <codecell>
 
-dist2 = 6378.388 * np.arccos(np.sin(lat1*np.pi/180.0)*np.sin(lat2*np.pi/180.0)+
+R = 6378.388
+dist2 = R * np.arccos(np.sin(lat1*np.pi/180.0)*np.sin(lat2*np.pi/180.0)+
                              np.cos(lat1*np.pi/180.0)*np.cos(lat2*np.pi/180.0)*
                              np.cos(lon2*np.pi/180.0-lon1*np.pi/180.0))
 print('Entfernung aus Seitencosinus:\t\t%.3fm (ca. %dkm)' % (dist2*1000.0, dist2))
+
+# <headingcell level=2>
+
+# Entfernung über Haversine Formel
+
+# <markdowncell>
+
+# Ist Seitencosinus, aber für kurze Entfernungen besser
+
+# <codecell>
+
+dlat=(lat1-lat2)*np.pi/180.0
+dlon=(lon1-lon2)*np.pi/180.0
+
+a = np.sin(dlat/2) * np.sin(dlat/2) + np.cos(lat1*np.pi/180.0) \
+        * np.cos(lat2*np.pi/180.0) * np.sin(dlon/2) * np.sin(dlon/2)
+c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+dist3 = R * c
+print('Entfernung aus Haversine:\t\t%.3fm (ca. %dkm)' % (dist3*1000.0, dist2))
 
 # <headingcell level=2>
 
@@ -121,7 +153,23 @@ print('Entfernung aus Seitencosinus:\t\t%.3fm (ca. %dkm)' % (dist2*1000.0, dist2
 # <codecell>
 
 diff = dist2-dist
-print('Berechnung über Pythagoras macht %.2fkm (%.2f%%) Fehler.' % (diff, np.abs(diff*100.0/dist2)))
+diff2= dist3-dist
+print('Berechnung über Pythagoras macht %.3fkm (%.3f%%) Fehler ggü Seitencosinus.' % (diff, np.abs(diff*100.0/dist2)))
+print('Berechnung über Pythagoras macht %.3fkm (%.3f%%) Fehler ggü Haversine.' % (diff2, np.abs(diff2*100.0/dist3)))
+
+# <headingcell level=1>
+
+# Convert Meters back to Lat/Lon (WGS84) via Pythagoras
+
+# <codecell>
+
+lat2b = lat1 + dy/arc
+lon2b = lon1 + dx/(arc*np.cos(lat))
+
+# <codecell>
+
+print('Differenz Lat: %.3fkm' % (lat2b-lat2))
+print('Differenz Lon: %.3fkm' % (lon2b-lon2))
 
 # <headingcell level=2>
 
